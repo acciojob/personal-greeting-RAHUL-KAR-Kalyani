2bc3d8f8b2233127b31987b304019cdba538562b
RK <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2024-06-18 02:53:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,18 @@
-
-import React from "react";
+import React, { useState } from 'react';
 import './../styles/App.css';
 
-const App = () => {
-  const [inputValue, setInputValue] = useState('');
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-  return (
-    <div>
-      <div>Enter your name</div>
-      <input type="text" value={inputValue} onChange={handleInputChange} />
-      <div>Hello {inputValue}</div>
-    </div>
-  )
+function App() {
+	const [inputValue, setInputValue] = useState('');
+	const handleInputChange = (e) => {
+		setInputValue(e.target.value);
+	};
+	return (
+		<div>
+			<p>Enter your name:</p>
+			<input type="text" value={inputValue} onChange={handleInputChange}/>
+			<p>Hello {inputValue}</p>
+		</div>
+	)
 }
 
 export default App
