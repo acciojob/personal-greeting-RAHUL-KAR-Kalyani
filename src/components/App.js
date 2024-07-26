ca0906e6ad7f706484fe7c0ddef00d731baba5e7
RK <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2024-07-26 06:01:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,13 @@
-// Greeting.test.js
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import './../styles/App.css';
 import Greeting from './Greeting';
 
-test('displays a personal greeting', () => {
-  render(<Greeting />);
-
-  // Check the initial rendering
-  expect(screen.getByText('Enter your name:')).toBeInTheDocument();
-  expect(screen.queryByText(/Hello /)).toBeNull();
-
-  // Simulate typing into the input
-  fireEvent.change(screen.getByRole('textbox'), { target: { value: 'n' } });
-
-  // Check the updated rendering
-  expect(screen.getByText('Hello n')).toBeInTheDocument();
-});
+function App() {
+	
+	return (
+		<div>
+			<Greeting/>			
+		</div>
+	)
+}
+export default App;
